4ea25f4183da44311952b59cbb13572ae98c600a
Higor <higoras.silva0@gmail.com>
2023-10-01 20:27:07 -0300

    MergeSort

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
 ﻿using System.Diagnostics;
+using System;
 class Program {
     static void Main(String[] args) {
 
-        Random random = new Random();
-        int tamanhoVetor = 1000000;
+        //Random random = new Random();
+        //int tamanhoVetor = 1000000;
 
-        int[]vetorAleatorio = new int[tamanhoVetor];
+        //int[]vetorAleatorio = new int[tamanhoVetor];
 
-        for (int i = 0; i < tamanhoVetor; i++){
-            vetorAleatorio[i] = random.Next(1,1000000);
-        }
+        //for (int i = 0; i < tamanhoVetor; i++){
+           // vetorAleatorio[i] = random.Next(1,1000000);
+       // }
         
         //foreach (int valor in vetorAleatorio){
           //  Console.WriteLine(valor);
         //}
-        BubbleSort(vetorAleatorio);
+        //BubbleSort(vetorAleatorio);
         //SelectionSort(vetorAleatorio);
         //InsertionSort(vetorAleatorio);
 
@@ -23,6 +24,15 @@ class Program {
         //{
             //Console.Write(vetorAleatorio[i] + " ");
         //}
+
+         int[] arr = { 12, 11, 13, 5, 6, 7 };
+        Console.WriteLine("Array original:");
+        PrintArray(arr);
+
+        Sort(arr, 0, arr.Length - 1);
+
+        Console.WriteLine("Array ordenado:");
+        PrintArray(arr);
 
         Console.ReadLine();
         
@@ -98,5 +108,80 @@ class Program {
         stopwatch.Stop();
         long tempoDecorridoEmMilissegundos = stopwatch.ElapsedMilliseconds;
         Console.WriteLine("Tempo decorrido: " + tempoDecorridoEmMilissegundos + " milissegundos");
+    }
+
+    public static void Merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+        {
+            leftArr[i] = arr[left + i];
+        }
+
+        for (int j = 0; j < n2; j++)
+        {
+            rightArr[j] = arr[middle + 1 + j];
+        }
+
+        int k = left;
+        int iIdx = 0, jIdx = 0;
+
+        while (iIdx < n1 && jIdx < n2)
+        {
+            if (leftArr[iIdx] <= rightArr[jIdx])
+            {
+                arr[k] = leftArr[iIdx];
+                iIdx++;
+            }
+            else
+            {
+                arr[k] = rightArr[jIdx];
+                jIdx++;
+            }
+            k++;
+        }
+
+        while (iIdx < n1)
+        {
+            arr[k] = leftArr[iIdx];
+            iIdx++;
+            k++;
+        }
+
+        while (jIdx < n2)
+        {
+            arr[k] = rightArr[jIdx];
+            jIdx++;
+            k++;
+        }
+    }
+
+    public static void Sort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+
+            Sort(arr, left, middle);
+            Sort(arr, middle + 1, right);
+
+            Merge(arr, left, middle, right);
+        }
+    }
+
+   
+
+    public static void PrintArray(int[] arr)
+    {
+        foreach (var num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
     }
 }
