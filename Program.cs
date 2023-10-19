2475a2d416c9835d9dff445c31fe1c0abdba611b
Higor <higoras.silva0@gmail.com>
2023-10-19 19:19:31 -0300

    com chamada para merge sort

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using System;
 class Program {
     static void Main(String[] args) {
 
-       // Random random = new Random();
-        //int tamanhoVetor = 5000000;
+       Random random = new Random();
+        int tamanhoVetor = 50000;
 
-        //int[]vetorAleatorio = new int[tamanhoVetor];
+        int[]vetorAleatorio = new int[tamanhoVetor];
 
-  //      for (int i = 0; i < tamanhoVetor; i++){
-    //        vetorAleatorio[i] = random.Next(1,75);
-      // }
+       for (int i = 0; i < tamanhoVetor; i++){
+           vetorAleatorio[i] = random.Next(1,5000);
+      }
 
        // tempo para vetor do tamanho 500: 0 milissegundos
         // tempo para vetor do tamanho 1000: 0 milissegundos 
@@ -31,16 +31,18 @@ class Program {
             //Console.Write(vetorAleatorio[i] + " ");
         //}
 
-        int[]  vetorAleatorio= { 3,6,1,2,3,6,4,2,3,1};
+        //int[]  vetorAleatorio= { 3,6,1,2,3,6,4,2,3,1};
         //Console.WriteLine("vetor original:");
         //Printvetor(vetorAleatorio);
 
-        //Sort(vetor, 0, vetor.Length - 1);
+        Sort(vetorAleatorio, 0, vetorAleatorio.Length - 1);// chamada para Mergesort
 
         //Console.WriteLine("vetor ordenado:");
         //Printvetor(vetor);
+
+
         
-        ContOrdena(vetorAleatorio);
+        //ContOrdena(vetorAleatorio);
         Console.ReadLine();
     
     }
@@ -166,6 +168,7 @@ class Program {
             jIdx++;
             k++;
         }
+        Printvetor(vetor);
     }
 
     public static void Sort(int[] vetor, int esquerda, int direita){
